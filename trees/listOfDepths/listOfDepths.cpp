823ddd7a7dcc182b9475fd68b91a0e729974e728
Eric Yu <echenyu@umich.edu>
2016-07-06 20:38:33 -0700

    ey - added problem to create linked lists for each depth of a tree. WIP - making it work for dfs

--- trees/listOfDepths/listOfDepths.cpp
+++ trees/listOfDepths/listOfDepths.cpp
@@ -4,10 +4,18 @@
 #include <vector>
 #include <queue>
 #include "../Tree.h"
+#include <map>
 
 using namespace std;
 
 vector<Node *> createListsForDepth(Node *head);
+vector<Node *> createListsForDepthHash(Node *head);
+void createListsForDepthHash_helper(Node *head, map<int, vector<Node *> > &nodesAtLevel, int count); 
+
+//          1
+//        2   2
+//       3 3 3 3 
+//		7
 
 int main() {
 	vector<int> treeValues; 
@@ -20,10 +28,10 @@ int main() {
 	treeValues.push_back(3); 
 	treeValues.push_back(3); 
 	treeValues.push_back(7); 
-
+	
 
 	Tree firstTree(treeValues); 
-	vector<Node *> levels = createListsForDepth(firstTree.get_head()); 
+	vector<Node *> levels = createListsForDepthHash(firstTree.get_head()); 
 	for(int i = 0; i < levels.size(); i++) {
 		Node *head = levels[i]; 
 		cout << "Level " << i << ":"; 
@@ -76,4 +84,44 @@ vector<Node *> createListsForDepth(Node *head) {
 	}
 
 	return nodesAtLevel; 
+}
+
+vector<Node *> createListsForDepthHash(Node *head) {
+	map<int, vector<Node *> > nodesAtLevel;
+	createListsForDepthHash_helper(head, nodesAtLevel, 1);  
+	vector<Node *> nodesAtLevel_vector; 
+	map<int, vector<Node *> >::iterator it;
+
+	for(it = nodesAtLevel.begin(); it != nodesAtLevel.end(); it++) {
+		vector<Node *> nodes = it->second; 
+		Node *prev = nullptr; 
+		Node *head = nodes[0]; 
+		for(int i = 0; i < nodes.size(); i++) {
+			
+			Node *current = nodes[i]; 
+			current->left = nullptr; 
+			if(prev) {
+				prev->left = current; 
+			}
+			prev = current; 
+		}
+		nodesAtLevel_vector.push_back(head);
+	}
+	return nodesAtLevel_vector; 
+}
+
+void createListsForDepthHash_helper(Node *head, map<int, vector<Node *> > &nodesAtLevel, int count) {
+	if(head == nullptr) {
+		return; 
+	}
+
+	if(nodesAtLevel.find(count) != nodesAtLevel.end()) {
+		vector<Node *> currentLevel = nodesAtLevel[count]; 
+	} else {
+		nodesAtLevel[count] = vector<Node *>(); 
+	}
+
+	nodesAtLevel[count].push_back(head); 
+	createListsForDepthHash_helper(head->left, nodesAtLevel, count+1); 
+	createListsForDepthHash_helper(head->right, nodesAtLevel, count+1); 
 }
