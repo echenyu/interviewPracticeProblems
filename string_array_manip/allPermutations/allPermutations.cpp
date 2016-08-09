016ac7591918c71e6cde46f6db09f2bdc6b72a97
Eric Yu <echenyu@umich.edu>
2016-08-08 21:37:53 -0700

    ey - added problem to print out all permutations

---
+++ string_array_manip/allPermutations/allPermutations.cpp
@@ -0,0 +1,30 @@
+//[1,2,3]
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> permutations;
+        vector<int> currentPath; 
+        permute_helper(nums, permutations, currentPath);   
+        return permutations; 
+    }
+    
+    void permute_helper(vector<int> &nums, vector<vector<int>> &permutations, vector<int> currentPath) {
+        if(nums.size() == 0) {
+            permutations.push_back(currentPath); 
+        }
+        
+        int index = 0; 
+        for(auto i : nums) {
+            nums.erase(nums.begin() + index); 
+            currentPath.push_back(i); 
+            
+            permute_helper(nums, permutations, currentPath); 
+            
+            nums.insert(nums.begin() + index, i); 
+            currentPath.erase(currentPath.begin() + currentPath.size() - 1); 
+            
+            index++; 
+        }
+    }
+};
